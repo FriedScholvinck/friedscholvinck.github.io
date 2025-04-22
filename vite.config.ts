ca4e94aa3e6ff72548727d5af03368f8086c76fb
fried <43989408+FriedScholvinck@users.noreply.github.com>
2025-04-22 15:32:17 +0200

    fix build

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 import { blogPostsPlugin } from "./vite-blog-plugin";
 
 // https://vitejs.dev/config/
@@ -14,8 +13,6 @@ export default defineConfig(({ mode }) => ({
   plugins: [
     react(),
     blogPostsPlugin(),
-    mode === 'development' &&
-    componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
